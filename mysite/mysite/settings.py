c6ca61bc23de9c723ba956597d373c640fe6b8cf
viharivnv <52350934+viharivnv@users.noreply.github.com>
2020-10-29 16:23:53 -0400

    Added Termbill app

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,19 +9,18 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%req2z7+rz02m-fe$lax!euv2hv0#p5qwhlk)@&pfu9k=4e_r-'
+SECRET_KEY = 'oqtf8+wg*gkf6%)2rzsun0@(+ab(8y%!bsw&-7dy2tovjrb_$m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'termbill.apps.TermbillConfig',
     'myR.apps.MyrConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webreg',
-    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,4 +121,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from myR import views as myviews
+from termbill import views as tbviews
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', user_views.login, name='login'),
+    path('', myviews.home, name='Home'),
+    path('Bill', tbviews.bill, name='Bill'),
 ]
