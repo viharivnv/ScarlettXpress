9d0c95ca43de24731938318b420ccaa571d5899a
viharivnv <52350934+viharivnv@users.noreply.github.com>
2020-11-05 18:03:31 -0500

    creating structures

--- mysite/myR/models.py
+++ mysite/myR/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Course(models.Model):
+    Index=models.IntegerField()
+    FullCode = models.CharField(max_length=10, primary_key=True)
+    Instructors = models.CharField(max_length=500)
+    Status = models.CharField(max_length=10)
+    MeetingTimes = models.CharField(max_length=100)
+    Capacity=models.IntegerField()
+    Enrolled = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'course'
+
+class Student(models.Model):
+    Student = models.CharField(max_length=50)
+    netId = models.IntegerField()
+    ruId = models.IntegerField()
+    credits = models.IntegerField()
+    gradelevel =  models.CharField(max_length=50)
+    CoursesTaken =  models.CharField(max_length=1000)
+    class Meta:
+        managed = False
+        db_table = 'students'
