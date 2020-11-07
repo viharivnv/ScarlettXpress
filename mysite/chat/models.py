7ec42331be079811319ca29203a2d9cd7378aadf
Snork <sachmathew@gmail.com>
2020-11-07 15:57:53 -0500

    updated chat html

--- mysite/chat/models.py
+++ mysite/chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Conversation(models.Model):
+    def getMessageList(self):
+        return "oof"
+
+class Message(models.Model):
+    sender_id = models.Charfield(max_length=10)
+    message_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+
+#class Student
