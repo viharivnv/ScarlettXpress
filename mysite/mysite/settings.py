9218f6b27a37c58b8a57244c32854b0163d1caaf
Aravind-PC <am2290@scarletmail.rutgers.edu>
2020-11-07 16:30:57 -0500

    Linked MongoDB

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'courseplanner.apps.CourseplannerConfig',
     'chat.apps.ChatConfig',
     'termbill.apps.TermbillConfig',
     'myR.apps.MyrConfig',
