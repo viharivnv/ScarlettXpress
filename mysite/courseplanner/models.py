f65b694b11ebf3a96630c1d4390ff2e36579ee4d
Aravind-PC <am2290@scarletmail.rutgers.edu>
2020-11-07 16:30:57 -0500

    Linked MongoDB

--- mysite/courseplanner/models.py
+++ mysite/courseplanner/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    Index=models.IntegerField()
+    FullCode = models.CharField(max_length=10, primary_key=True)
+    Instructors = models.CharField(max_length=500)
+    Status = models.CharField(max_length=10)
+    MeetingTimes = models.CharField(max_length=100)
+    Capacity=models.IntegerField()
+    Enrolled = models.IntegerField()
+
+    class Meta:
+        managed = True
+        db_table = 'courseplanner_course'
+
+class Student(models.Model):
+    Student = models.CharField(max_length=50)
+    netId = models.IntegerField()
+    ruId = models.IntegerField()
+    credits = models.IntegerField()
+    gradelevel =  models.CharField(max_length=50)
+    CoursesTaken =  models.CharField(max_length=1000)
+    class Meta:
+        managed = True
+        db_table = 'courseplanner_students'
