c8895aa33809ad98960c5262e59e1cd0ff03f82a
fareselkhouli <fareselkhouli@gmail.com>
2020-11-08 13:29:22 -0500

    database

--- mysite/myR/models.py
+++ mysite/myR/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 
 class Course(models.Model):
     FullCode = models.CharField(max_length=10, primary_key=True)
-    Index=models.IntegerField(max_length=5)
-    Title = models.CharField(max_length=500,blank=True, null=True)
+    Index = models.IntegerField()
+    Title = models.CharField(max_length=500, blank=True, null=True)
     Instructors = models.CharField(max_length=500)
-    courseCredits = models.IntegerField(blank=True, null=True)
-    MeetingType = models.CharField(max_length=100,blank=True, null=True)
+    Credits = models.IntegerField(default=0)
+    MeetingType = models.CharField(max_length=100, blank=True, null=True)
     Status = models.BooleanField(default=True)
     Enrolled = models.IntegerField(blank=True, null=True)
     Capacity = models.IntegerField(blank=True, null=True)
@@ -18,16 +18,16 @@ class Course(models.Model):
 
 
 class Student(models.Model):
-    netid = models.IntegerField(max_length=6,blank=True, null=True)
-    RUID = models.IntegerField(max_length=50,blank=True, null=True)
+    netid = models.IntegerField(blank=True, null=True)
+    RUID = models.IntegerField(blank=True, null=True)
   # Password = models.CharField(max_length=20)
     FirstName = models.CharField(max_length=50,blank=True, null=True)
     LastName = models.CharField(max_length=50,blank=True, null=True)
     DegreeLevel = models.CharField(max_length=50,blank=True, null=True)
-    CreditsRegistered = models.IntegerField(max_length=15,blank=True, null=True)
-    CreditsTaken = models.IntegerField(max_length=15,blank=True, null=True)
-    CoursesRegistered = models.IntegerField(max_length=15,blank=True, null=True)
-    CoursesTaken =  models.IntegerField(max_length=15,blank=True, null=True)
+    CreditsRegistered = models.IntegerField(blank=True, null=True)
+    CreditsTaken = models.IntegerField(blank=True, null=True)
+    CoursesRegistered = models.IntegerField(blank=True, null=True)
+    CoursesTaken =  models.IntegerField(blank=True, null=True)
     def __str__(self):
         return self.FirstName + " " + self.LastName
 
