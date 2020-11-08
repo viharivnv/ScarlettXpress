2ac28989977202e56de79346dc568038c98488f0
Aravind-PC <am2290@scarletmail.rutgers.edu>
2020-11-08 18:24:40 -0500

    Edited views.py

--- mysite/courseplanner/views.py
+++ mysite/courseplanner/views.py
@@ -3,14 +3,4 @@ from django.shortcuts import render
 from .models import Course, Student
 
 def planner(request):
-    myclient = MongoClient('localhost', 27017)
-    mydb = myclient["RUDB"]
-    mycol = mydb["courseplanner_courses"]
-    context = {}
-    i = 1
-    for doc in mycol.find():
-        context["course"+i] = doc
-        i += 1
-    context["totalCourses"] = i
-
-    return render(request, 'courseplanner.html', context)
+    return render(request, 'courseplanner.html')
