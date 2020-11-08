8bd56c1174af355d8e101dd293fc17cfcb1c5605
viharivnv <52350934+viharivnv@users.noreply.github.com>
2020-11-08 18:03:36 -0500

    termbill

--- mysite/termbill/views.py
+++ mysite/termbill/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from myR.models import *
+
 def bill(request):
-    if not request.user.is_authenticated:
-        return render(request, 'users/login.html')
-    username=request.user.username
-    record = Student.objects.filter(netId=username)
-    credits=record.credits
-    credits*=3950
-    data={'credits':credits}
-    return render(request, 'termbill/termbill.html',data)
+    credits = 1
+    credits *= 3950
+
+    data = [
+        {'credits': str(credits)
+         }
+    ]
+    context={
+        'data':data
+    }
+    return render(request, 'termbill/termbill.html', context)
