f02e6f854e882b3d29e217df157ceef4c1784ef5
viharivnv <52350934+viharivnv@users.noreply.github.com>
2020-11-08 20:03:15 -0500

    Updating termbill

--- mysite/termbill/views.py
+++ mysite/termbill/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 
+campusFee=1144.95
+schoolFee=104.75
+dbc=119.99
+misc=75.00
+CompFee=171
+pirg=11.2
 def bill(request):
     credits = 1
-    credits *= 3950
-
+    tution = credits * 3950
+    fees = campusFee + schoolFee + tution + dbc + misc + CompFee
+    paid = 0
+    balance = fees - paid
     data = [
-        {'credits': str(credits)
-         }
+        {
+            'ComputerFee': '$171.00',
+            'PIRG': '$11.20',
+            'CampusFee': '$1144.95',
+            'SchoolFee': '$104.75',
+            'TutionFees': str(tution),
+            'DigitalBookCharge': '$8629.89',
+            'MISC': '$75.00',
+            'TotalCharges': str(fees),
+            'TotalPayments': str(paid),
+            'BalanceDue': str(balance)
+
+        }
     ]
-    context={
-        'data':data
+    context = {
+        'data': data
     }
     return render(request, 'termbill/termbill.html', context)
