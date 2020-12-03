cf5cc969b4bc22dcd7ef37237c162ca5c41803bb
viharivnv <52350934+viharivnv@users.noreply.github.com>
2020-12-03 13:07:40 -0500

    Added Duedate

--- mysite/myR/models.py
+++ mysite/myR/models.py
@@ -28,5 +28,6 @@ class Student(models.Model):
     CreditsTaken = models.IntegerField()
     CoursesRegistered = models.IntegerField()
     CoursesTaken =  models.IntegerField()
+    DueDate = models.DateField()
     def __str__(self):
         return self.FirstName + " " + self.LastName
