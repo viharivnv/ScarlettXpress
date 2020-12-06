d826a170d09221f6dd0b69c9d95334384443c4c6
Aravind-PC <am2290@scarletmail.rutgers.edu>
2020-12-06 11:22:58 -0500

    Implemented timetable functionality

--- mysite/myR/urls.py
+++ mysite/myR/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('bill/', tbviews.bill, name='Bill'),
     path('register/', wbviews.index, name='Index'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
